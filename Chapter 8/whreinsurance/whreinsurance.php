ec7ebff103d719cf099674459e76e403905a8b33
Louis AUTHIE <65625876+LouisAUTHIE@users.noreply.github.com>
2024-05-27 21:44:21 +0200

    Update whreinsurance.php

--- Chapter 8/whreinsurance/whreinsurance.php
+++ Chapter 8/whreinsurance/whreinsurance.php
@@ -18,7 +18,7 @@ class WHReinsurance extends Module
         $this->need_instance = 0;
         $this->ps_versions_compliancy = [
             'min' => '1.7.0',
-            'max' => '8.99.99',
+            'max' => '9.99.99',
         ];
         $this->bootstrap = false;
 
